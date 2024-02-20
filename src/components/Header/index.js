13db92c04886232b2f5979f8fe50fd0b3b70fad7
Matheus Italo <haiganbr@gmail.com>
2024-02-20 15:07:47 -0300

    DevFlix React+API+Tostify+axios

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import "./header.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link className="logo" to="/">
+        DevFlix
+      </Link>
+      <Link className="favoritos" to="/favoritos">
+        Meus filmes
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
